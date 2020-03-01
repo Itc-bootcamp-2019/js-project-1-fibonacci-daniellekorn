214ed0fba84d921d8f1d00575c92eb1fc20daa58
Danielle Korn <daniella.korn@gmail.com>
2020-03-01 18:23:58 +0200

    milestone 3 (revised)

--- js/fibo.js
+++ js/fibo.js
@@ -1,5 +1,19 @@
-x = 8;
-y = 21;
+function fibonacci(x) {
+	let first = 0;
+	let second = 1;
+	let y;
+
+	for (let i = 2; i <= x; i++) {
+		y = first + second;
+		first = second;
+		second = y;
+	}
+	return y;
+}
+
+/* Declare variables to run function*/
+let x = 10;
+let y = fibonacci(x);
 
 const docX = document.getElementById("x");
 const docY = document.getElementById("y");
