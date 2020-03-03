858ce9dacc6a684881d0e492bd92f5a2e6e0ca77
Danielle Korn <daniella.korn@gmail.com>
2020-03-03 16:00:03 +0200

    retrieves fib on click and prints only to console

--- js/main.js
+++ js/main.js
@@ -1,31 +1,20 @@
 const docY = document.getElementById("y");
-const x = 8;
-const x2 = document.getElementById("x").value;
+let x = 8;
 
-const ourServer = "http://localhost:5050/fibonacci/" + x2;
-//IDEA: + .toString;
+calcButton.addEventListener("click", () => {
+	x = document.getElementById("x").value;
 
-fetch(ourServer)
-	.then(resp => {
-		//I think notation is alt to saying function(resp)
-		return resp.json();
-	}) // Transform the data into json (which is a format for storing and transporting data)
-	.then(data => {
-		console.log(data);
-		docY.innerText = data[0];
+	let ourServer = "http://localhost:5050/fibonacci/" + x;
 
-		//let y = data.results;
-	});
+	fetch(ourServer)
+		.then(resp => {
+			//I think notation is alt to saying function(resp)
+			return resp.json();
+		}) // Transform the data into json (which is a format for storing and transporting data)
+		.then(data => {
+			console.log(data);
+			docY.innerText = data[0];
 
-console.log(x2);
-//function fib(x) {}
-
-//calcButton.addEventListener("click", () => {
-//	fib(x);
-//});
-
-//	let x = document.getElementById("x").value;
-//	let y = fibonacci(x);
-//
-//
-//});
+			//let y = data.results;
+		});
+});
