8e0a74fa184929db6ae776792fbabcfe42d8fce9
Danielle Korn <daniella.korn@gmail.com>
2020-03-04 16:51:57 +0200

    propper notices at propper times; added function outside to make more readable

--- js/main.js
+++ js/main.js
@@ -1,24 +1,29 @@
 const docY = document.getElementById("y");
 let x = 8;
 
+function serverRequest() {
+	let ourServer = "http://localhost:5050/fibonacci/" + x;
+
+	document.getElementById("errorMessage").style.display = "none";
+	fetch(ourServer)
+		.then(resp => {
+			//I think notation is alt to saying function(resp)
+			return resp.json();
+		}) // Transform the data into json (which is a format for storing and transporting data)
+		.then(data => {
+			console.log(data);
+			docY.innerText = data.result;
+		});
+}
+
 calcButton.addEventListener("click", () => {
 	x = document.getElementById("x").value;
 
-	let ourServer = "http://localhost:5050/fibonacci/" + x;
-
 	if (x > 50) {
+		document.getElementById("y").style.display = "none";
 		document.getElementById("errorMessage").style.display = "block";
 		document.getElementById("error").innerHTML = "Can't be larger than 50";
 	} else {
-		document.getElementById("errorMessage").style.display = "none";
-		fetch(ourServer)
-			.then(resp => {
-				//I think notation is alt to saying function(resp)
-				return resp.json();
-			}) // Transform the data into json (which is a format for storing and transporting data)
-			.then(data => {
-				console.log(data);
-				docY.innerText = data.result;
-			});
+		serverRequest();
 	}
 });
