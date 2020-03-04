b23672ed31d788ddfa61a1aba8f753fd36c149e1
Danielle Korn <daniella.korn@gmail.com>
2020-03-04 16:14:07 +0200

    Over 50 error message working)

--- js/main.js
+++ js/main.js
@@ -6,15 +6,19 @@ calcButton.addEventListener("click", () => {
 
 	let ourServer = "http://localhost:5050/fibonacci/" + x;
 
-	fetch(ourServer)
-		.then(resp => {
-			//I think notation is alt to saying function(resp)
-			return resp.json();
-		}) // Transform the data into json (which is a format for storing and transporting data)
-		.then(data => {
-			console.log(data);
-			docY.innerText = data.result;
-
-			//let y = data.results;
-		});
+	if (x > 50) {
+		document.getElementById("errorMessage").style.display = "block";
+		document.getElementById("error").innerHTML = "Can't be larger than 50";
+	} else {
+		document.getElementById("errorMessage").style.display = "none";
+		fetch(ourServer)
+			.then(resp => {
+				//I think notation is alt to saying function(resp)
+				return resp.json();
+			}) // Transform the data into json (which is a format for storing and transporting data)
+			.then(data => {
+				console.log(data);
+				docY.innerText = data.result;
+			});
+	}
 });
